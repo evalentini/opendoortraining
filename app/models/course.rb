6303af7f3490bc45b2564f63677c3fc7a593920a
Evan Valentini <evan.valentini@gmail.com>
2013-11-30 10:57:05 -0600

    added some basic video support

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,15 @@ class Course < ActiveRecord::Base
   def self.careerCourseList(career="Banking")
     Course.where(:career => career).order("name asc")
   end
+  
+  def self.courseRecordingLink
+    s3=AWS::S3.new(:access_key_id => 'AKIAICAG7IOKG5O7EDJA', :secret_access_key => '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')
+    begin
+      recordingLink=s3.buckets['opendoortraining'].objects["testmovie.mp4"].url_for(:get).to_s
+    rescue
+      recordingLink="#"
+    end 
+    recordingLink 
+  end
+  
 end
